f4af16728b521df4c686f95ec2d5ef372fc8d432
MASTARYzzz <43580849+MASTARYzzz@users.noreply.github.com>
2020-02-24 19:43:45 +0300

    created Tower

--- Player.py
+++ Player.py
@@ -1,1 +1,17 @@
+class Player:
+    def __init__(self, _name, _tower, _shop):
+        self.name = _name
+        self.game_score = 0
+        self.tower = _tower
+        self.cards = []
+        self.buildings = []
+        self.player_gold = 0
+        self.shop = _shop
+
+    def lose(self):
+        pass
+    def win(self):
+        pass
+
+
 
