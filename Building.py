e9f2f174a39655182f72f55c41fcffeee6145e51
ilkornej <ilkornej@yandex.ru>
2020-02-24 19:57:31 +0300

    Добавил в Building.py class Building и дочерние классы AttackBuilding и RepairBuilding и абстрактный класс SpecialBuilding

--- Building.py
+++ Building.py
@@ -5,12 +5,46 @@
 # Наследуемые классы:
 #   Атакующее здание
 #   Ремонтирующее здание
-#   Снижающее урон здание
-#   Приносящее золото здание
+#   Специальное здание
+
 import pygame
 import random
 import sys
 import math
 
 class Building:
+    price = 0
+    sprite = 0
+    owner = 0
     def __init__(self):
+        self.price = 0
+        self.sprite = 0
+        self.owner = 0
+    def useBuilding(self):
+        pass
+
+class AttackBuilding(Building):
+    def __init__(self):
+        self.price = price
+        self.sprite = sprite
+        self.owner = owner
+    def useBuilding(self):
+        pass
+
+class RepairBuilding(Building):
+    def __init__(self):
+        self.price = price
+        self.sprite = sprite
+        self.owner = owner
+    def useBuilding(self):
+        pass
+
+class SpecialBuilding(Building):
+    using_ref = 0
+    def __init__(self):
+        self.price = price
+        self.sprite = sprite
+        self.owner = owner
+        self.using_ref = using_ref
+    def useBuilding(self):
+        pass
