fe2cace045e3cdebcd9f5b572d6dc7633ef25891
1111 <olegkanprivet@mail.ru>
2020-02-24 20:04:31 +0300

    Shop_1

--- Shop.py
+++ Shop.py
@@ -4,8 +4,13 @@
 #   Стоимость - cost_card
 #   Изображение магазина с картами
 #   Продажа карт - sell_card
-import pygame as p
-import random as r
+import pygame
+import random
 import sys
-import math as m
+import math
 
+class Shop:
+    def __init__(self):
+        self.image=font.render(str(random.randint(100,1000)), 1, [255, 0, 0])
+        self.width=self.image.get_width()
+    def sellWeakCard(self, x, y, ):
