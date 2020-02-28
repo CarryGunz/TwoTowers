974ca6d61d90d6f7c65523d780bee5cbf37edd80
MASTARYzzz <43580849+MASTARYzzz@users.noreply.github.com>
2020-02-28 21:27:01 +0300

    created Menu

--- Button.py
+++ Button.py
@@ -1,1 +1,25 @@
+import pygame
 
+class Button:
+    def __init__(self, posx, posy, button, m_on_button,screen_):
+        self.posx = posx
+        self.posy = posy
+        self.is_glowing = True
+        self.buttons = [button, m_on_button]
+        self.screen = screen_
+    def show(self):
+        self.on_object()
+        if self.is_glowing:
+            self.screen.blit(self.buttons[0], (self.posx, self.posy))
+
+        else:
+            self.screen.blit(self.buttons[1], (self.posx, self.posy))
+
+    def on_object(self):
+        cx, cy = pygame.mouse.get_pos()
+        if((cy > self.posy and cy < (self.posy + 50)) and (cx > self.posx and cx < self.posx + 300)):
+            if self.is_glowing == True:
+                self.is_glowing = False
+
+        else:
+            self.is_glowing = True
--- Main.py
+++ Main.py
@@ -5,8 +5,22 @@ import math
 import Player
 import Tower
 gameBoard = pygame.image.load('images/gameBoard.png')
+menuImage = pygame.image.load('images/menuImage.png')
+playButton = pygame.image.load('images/playButton.png')
+settingsButton = pygame.image.load('images/settingsButton.png')
+quitButton = pygame.image.load('images/quitButton.png')
 
+class Menu:
+    def __init__(self):
+        pass
+    def showMenu(self):
+        win.blit(menuImage, (0,0))
+        win.blit(playButton, (456,200))
+        win.blit(settingsButton, (456, 350))
+        win.blit(quitButton, (456, 500))
+        pygame.display.update()
 player = Player
+main_menu = Menu
 pygame.init()
 win = pygame.display.set_mode((1280,720))
 
@@ -16,7 +30,7 @@ def drawGame():
 def main():
     game_started = True
     while game_started:
-        drawGame()
+        main_menu.showMenu(main_menu)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_started = False
