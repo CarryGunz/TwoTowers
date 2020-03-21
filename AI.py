f16cedd19d2d3c5176d78b952e24d6d03ffec64b
Артём <artem.krilof2002@mail.ru>
2020-03-21 18:37:28 +0300

    Немного прописал простенький ИИ

--- AI.py
+++ AI.py
@@ -1,3 +1,6 @@
+import random
+import Card
+import AllCards
 class AI:
     def __init__(self, shop):
         self.tower = None
@@ -7,3 +10,73 @@ class AI:
         self.shop = shop
         self.turnIncome = 1
         self.opponent = None
+
+    def TurnAI(self):
+        #Buy cards
+
+        rest = self.player_gold % 8
+        amount = self.player_gold // 8
+        self.player_gold = self.player_gold - amount*8
+
+        for i in range(0, amount):
+            #self.cards.append() gold
+            pass
+
+        rest = self.player_gold % 4
+        amount = self.player_gold // 4
+        self.player_gold = self.player_gold - amount * 4
+
+        for i in range(0, amount):
+            self.cards.append(AllCards.silver_cards[random.randint(0, len(AllCards.silver_cards))].cloneCard())
+            pass
+
+        rest = self.player_gold % 2
+        amount = self.player_gold // 2
+        self.player_gold = self.player_gold - amount * 2
+
+        for i in range(0, amount):
+            self.cards.append(AllCards.bronze_cards[random.randint(0, len(AllCards.bronze_cards))].cloneCard())
+            pass
+
+        rest = self.player_gold % 1
+        amount = self.player_gold // 1
+        self.player_gold = self.player_gold - amount * 1
+
+        for i in range(0, amount):
+            self.cards.append(AllCards.wooden_cards[random.randint(0, len(AllCards.wooden_cards))].cloneCard())
+            pass
+
+        #Use cards
+        k = 100 - self.tower.height  # 100 - max height
+        max_effect = None
+        max_effect_card = None
+        if k < random.randint(0, 100):# if ... do heal
+            for card in self.cards:
+                for effect in card.effects:
+                    if effect.effect == Card.addRepairEffect:
+                        if max_effect == None:
+                            max_effect = effect
+                            max_effect_card = card
+                            continue
+
+                        if max_effect.effect_power < effect.effect_power:
+                            max_effect = effect
+                            max_effect_card = card
+
+            max_effect_card.useCard()
+            return
+        else:
+            for card in self.cards:
+                for effect in card.effects:
+                    if effect.effect == Card.Card.addAttackEffect:
+                        if max_effect == None:
+                            max_effect = effect
+                            max_effect_card = card
+                            continue
+
+                        if max_effect.effect_power < effect.effect_power:
+                            max_effect = effect
+                            max_effect_card = card
+
+            max_effect_card.useCard()
+            return
