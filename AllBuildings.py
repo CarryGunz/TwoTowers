b0b803c3647520e91901bc746617ee322ce3cb21
MASTARYzzz <43580849+MASTARYzzz@users.noreply.github.com>
2020-03-26 20:35:05 +0300

    Исправил ошибки со зданием

---
+++ AllBuildings.py
@@ -0,0 +1,9 @@
+import Building
+import Sprite
+import pygame
+
+building1 = Building.Building(Sprite.Sprite(pygame.image.load('images/ArcherTowerBuilding.png')), Building.archerTowerEffect, 1)
+
+buildings = [building1]
+
+
